8f75c7f47c351e45536ad75fa5df75a33e1ffd6c
Arsh-Git1 <124809223+Arsh-Git1@users.noreply.github.com>
2024-05-22 18:27:06 +0530

    Add files via upload

---
+++ Python project/comments_mathoperator.py
@@ -0,0 +1,8 @@
+summed = 2 + 3
+difference = 10 - 8
+divided = 1000 / 10
+product = 7 * 5
+exponent = 3 ** 8
+floored = 10 // 3
+mod = 17 % 15  # could have used 3, 5, or 15 to get 2 as a result
+print(exponent)
---
+++ Python project/grocery_purchase.py
@@ -0,0 +1,14 @@
+# Variables holding the prices of the six items:
+
+# All of the variables were multiplied by 100 to make them into integers so that there would be no approximation errors
+# when they were added together.
+
+pasta = 16.68 * 100  # penne 16 oz, pack of 12
+sauce = 6.98 * 100  # Arrabiata sauce 24oz
+garlic = 16.78 * 100  # 20 pack garlic clove
+seasoning = 15.26 * 100  # Italian Seasoning
+bread = 3.00 * 100  # Baguette twin pack
+meatballs = 4.39 * 100  # 12 oz bag of meatballs
+# subtotal is the sum of all prices before any sales taxes or discounts are added
+subtotal = (pasta + sauce + garlic + seasoning + bread + meatballs) / 100
+print(subtotal)
